d2a45cf7f21ba6bdbd2d934466b4098308301f77
kimyang1997 <yang.lin1@northeastern.edu>
2024-01-04 19:14:20 +0700

    updating our directory page with directory component and category item component

--- src/App.js
+++ src/App.js
@@ -1,57 +1,48 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-class App extends Component{
-  constructor(){
-    super();
+const App = () => {
+  const [searchField, setSearchField] = useState(''); // [value, setValue]
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] = useState(monsters);
 
-    this.state = {
-      monsters: [],
-      searchField: '',
-    }; 
-  }
-
-  componentDidMount(){
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return {monsters: users};
-          }
-        )
-    )
-  }
+      .then((users) => setMonsters(users));
+  }, []); 
+  // () => {} the effect we want to happen   [] contains dependencies (state values)
+  // when the state value change is when I'm going to run this callback function
+  // [] empty means nothing is going to change, only run it one time
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(() => {
-        return { searchField };
-      });
-  }
-
-  render() {
-
-    const { monsters, searchField} = this.state;
-    const { onSearchChange } = this;
-
-    const filteredMonsters = monsters.filter((monster) => {
+  useEffect(() => {
+    const newFilteredMonsters = monsters.filter((monster) => {
       return monster.name.toLocaleLowerCase().includes(searchField);
     });
+    setFilterMonsters(newFilteredMonsters);
+  }, [monsters, searchField])
 
-    return (
-      <div className="App">      
+
+  const onSearchChange = (event) => {
+      const searchFieldString = event.target.value.toLocaleLowerCase();
+      setSearchField(searchFieldString);
+    }
+
+  return (
+    <div className="App">      
         <h1 className='app-title'>Monsters Rolodex</h1>
+
         <SearchBox 
         className='monsters-search-box'
         onChangeHandler={onSearchChange} 
         placeholder='search monsters' />
-        <CardList monsters={filteredMonsters} />
-      </div>
-    );
-  }
-}
+        <CardList monsters={filteredMonsters} 
+        />
+
+    </div>
+  );
+};
 
 export default App;
